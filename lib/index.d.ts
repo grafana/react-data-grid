37c62d5a56997251c08d8c83d38adffdf532178f
Paul Marbach <paul.marbach@grafana.com>
2025-05-23 13:52:25 -0400

    (react-18) build from commit 9e49997ef0928e568bc2b8b4ee951357f04b3aba

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,13 @@
-import * as react1 from "react";
+import * as react0 from "react";
+import * as react13 from "react";
 import * as react12 from "react";
-import * as react8 from "react";
 import { Key, ReactElement, ReactNode, RefAttributes } from "react";
-import * as react_jsx_runtime0 from "react/jsx-runtime";
+import * as react_jsx_runtime1 from "react/jsx-runtime";
 import * as react_jsx_runtime9 from "react/jsx-runtime";
 import * as react_jsx_runtime10 from "react/jsx-runtime";
 import * as react_jsx_runtime7 from "react/jsx-runtime";
+import * as react_jsx_runtime6 from "react/jsx-runtime";
 import * as react_jsx_runtime4 from "react/jsx-runtime";
-import * as react_jsx_runtime5 from "react/jsx-runtime";
 
 //#region src/types.d.ts
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
@@ -443,11 +443,11 @@ declare function TreeDataGrid<R, SR = unknown, K extends Key = Key>({
   onExpandedGroupIdsChange,
   groupIdGetter: rawGroupIdGetter,
   ...props
-}: TreeDataGridProps<R, SR, K>): react_jsx_runtime0.JSX.Element;
+}: TreeDataGridProps<R, SR, K>): react_jsx_runtime1.JSX.Element;
 
 //#endregion
 //#region src/DataGridDefaultRenderersContext.d.ts
-declare const DataGridDefaultRenderersContext: react1.Context<Maybe<Renderers<any, any>>>;
+declare const DataGridDefaultRenderersContext: react0.Context<Maybe<Renderers<any, any>>>;
 
 //#endregion
 //#region src/Row.d.ts
@@ -482,7 +482,7 @@ declare function ToggleGroup<R, SR>({
 
 //#endregion
 //#region src/cellRenderers/renderValue.d.ts
-declare function renderValue<R, SR>(props: RenderCellProps<R, SR>): react12.ReactNode;
+declare function renderValue<R, SR>(props: RenderCellProps<R, SR>): react13.ReactNode;
 
 //#endregion
 //#region src/cellRenderers/SelectCellFormatter.d.ts
@@ -498,7 +498,7 @@ declare function SelectCellFormatter({
   onChange,
   'aria-label': ariaLabel,
   'aria-labelledby': ariaLabelledBy
-}: SelectCellFormatterProps): react8.ReactNode;
+}: SelectCellFormatterProps): react12.ReactNode;
 
 //#endregion
 //#region src/editors/textEditor.d.ts
@@ -515,13 +515,13 @@ declare function renderHeaderCell<R, SR>({
   column,
   sortDirection,
   priority
-}: RenderHeaderCellProps<R, SR>): string | react_jsx_runtime4.JSX.Element;
+}: RenderHeaderCellProps<R, SR>): string | react_jsx_runtime6.JSX.Element;
 
 //#endregion
 //#region src/sortStatus.d.ts
 declare function renderSortIcon({
   sortDirection
-}: RenderSortIconProps): react_jsx_runtime5.JSX.Element | null;
+}: RenderSortIconProps): react_jsx_runtime4.JSX.Element | null;
 declare function renderSortPriority({
   priority
 }: RenderSortPriorityProps): number | undefined;
